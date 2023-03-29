a76726fc95a6d103218ffa532debfa0fa36ccfa6
Amaya shaji <amayaji.s@pukkacodes.com>
2023-03-29 16:33:46 +0530

    created first react application of Expense chart

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import ExpenseItem from "./Components/ExpenseItem";
+function App() {
+  return (
+    <div className="App">
+      <h1>Let's Learn React</h1>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
