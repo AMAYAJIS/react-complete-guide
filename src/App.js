93516a037a05239a4c55a8877f00bf5bde7f7c06
Amaya shaji <amayaji.s@pukkacodes.com>
2023-03-30 12:15:54 +0530

    Reusablity of components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
     <div className="App">
       <h1>Let's Learn React</h1>
       <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,10 +1,13 @@
 import './ExpenseItem.css'
 function ExpenseItem(){
+    const expenseDate = new Date (2021, 2, 28);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.67;
     return (<div className ="expense-item">
-        <div>March 28th 2021</div>
-        <h2>Car Insurance</h2>
+        <div>{expenseDate.toISOString()}</div>
+        <h2>{expenseTitle}</h2>
         <div className ="expense-item-description">
-            <div className="expense-item__price">$294.67</div>
+            <div className="expense-item__price">${expenseAmount}</div>
         </div>
     </div>);
 }
