c473fd17dd86f508179af679ccf64c35942ffb38
Amaya shaji <amayaji.s@pukkacodes.com>
2023-04-05 10:05:04 +0530

    how to add events with usestae react hook

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -2,13 +2,16 @@
 import './ExpenseItem.css';
 import Card from "../UI/Card"
 import ExpenseDate from './ExpenseDate';
-
+import React,{ useState } from 'react';
 const ExpenseItem =(props) =>{
-  const clickHandler= () => {console.log('clicked')}
+
+  const [title,setTitle] = useState(props.title);
+  const clickHandler= () => {setTitle('Updated!!');
+                             console.log(title);}
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
-      <h2>{props.title}</h2>
+      <h2>{title}</h2>
       <div className="expense-item__description">
         <div className="expense-item__price">${props.amount}</div>
       </div>
